dd9014c3efc279a94c2a2290fe2ef588fd0f85cc
Nauara Melo <nauaramelo@gmail.com>
2020-08-06 17:14:31 -0300

    Add router phone number

--- src/router/UserRouter.ts
+++ src/router/UserRouter.ts
@@ -6,4 +6,5 @@ export const userRouter = express.Router();
 userRouter.post("/", new UserController().signUp);
 userRouter.post("/cpf", new UserController().addCpf);
 userRouter.post("/full-name", new UserController().addFullName);
-userRouter.post("/birthday", new UserController().addBirthday)
+userRouter.post("/birthday", new UserController().addBirthday);
+userRouter.post("/phone-number", new UserController().addPhoneNumber)
