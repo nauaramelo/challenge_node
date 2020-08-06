86cb51ea7ad6f5a6f6f477b839a48ae62ac52cfb
Nauara Melo <nauaramelo@gmail.com>
2020-08-06 00:11:11 -0300

    Change expire in and return of verify

--- src/services/tokenGenerator.ts
+++ src/services/tokenGenerator.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export class TokenGenerator {
-  private static expiresIn: number = 1200;
+  private static expiresIn: string = "10h";
 
   public generate = (input: AuthenticationData): string => {
     const newToken = jwt.sign(
@@ -18,10 +18,10 @@ export class TokenGenerator {
     return newToken;
   };
 
-  public verify(token: string) {
+  public verify(token: string): string {
     const payload = jwt.verify(token, process.env.JWT_KEY as string) as any;
-    const result = { id: payload.id, role: payload.role };
-    return result;
+
+    return payload.id;
   }
 }
 
